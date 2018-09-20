7522d83adcde99ba03ddcdfdfabd856338d47d57
felipegetulio <felipegetulio15@gmail.com>
2018-09-20 12:59:48 -0400

    Court Counter - Finished

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,72 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA;
+    int scoreTeamB;
+    TextView scoreViewTeamA;
+    TextView scoreViewTeamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    private void initialize(){
+        scoreTeamA = scoreTeamB = 0;
+
+        scoreViewTeamA = findViewById(R.id.team_a_score);
+        scoreViewTeamB = findViewById(R.id.team_b_score);
+
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void add3PointsToTeamA(View view){
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void add2PointsToTeamA(View view){
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void freeThrowToTeamA(View view){
+        displayForTeamA(++scoreTeamA);
+    }
+
+    public void add3PointsToTeamB(View view){
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void add2PointsToTeamB(View view){
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void freeThrowToTeamB(View view){
+        displayForTeamB(++scoreTeamB);
+    }
+
+    public void resetValues(View view){
+        scoreTeamA = scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void displayForTeamA(int score) {
+        scoreViewTeamA.setText(String.valueOf(score));
+    }
+
+    public void displayForTeamB(int score) {
+        scoreViewTeamB.setText(String.valueOf(score));
     }
 }
