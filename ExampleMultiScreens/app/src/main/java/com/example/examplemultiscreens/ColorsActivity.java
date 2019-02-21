8143be7078e27a2c1eb71dadd8088b1d79a69585
Felipe Nascimento <felipegetulio15@gmail.com>
2019-02-21 15:50:32 -0400

    Miwok Project Finished

--- ExampleMultiScreens/app/src/main/java/com/example/examplemultiscreens/ColorsActivity.java
+++ ExampleMultiScreens/app/src/main/java/com/example/examplemultiscreens/ColorsActivity.java
@@ -1,13 +1,39 @@
 package com.example.examplemultiscreens;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
+
+    private ArrayList<Word> words = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+
+        initialize();
+        configureAdapter();
+    }
+
+    void configureAdapter(){
+        WordAdapater wordAdapater = new WordAdapater(this, words);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(wordAdapater);
+    }
+
+    void initialize(){
+
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
     }
 }
--- ExampleMultiScreens/app/src/main/java/com/example/examplemultiscreens/FamilyActivity.java
+++ ExampleMultiScreens/app/src/main/java/com/example/examplemultiscreens/FamilyActivity.java
@@ -1,13 +1,44 @@
 package com.example.examplemultiscreens;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
+
+    private ArrayList<Word> words = new ArrayList<>();
+
+    int [][] matriz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+        initialize();
+        configureAdapter();
+    }
+
+    void configureAdapter(){
+        WordAdapater wordAdapater = new WordAdapater(this, words);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(wordAdapater);
+    }
+
+    void initialize(){
+
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
     }
 }
